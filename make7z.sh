9db7a6a13b866ad3449003709b043a216fc2d6f6
gnzhoutian <gnzhoutian@qq.com>
2025-09-07 09:05:51 +0800

    clean repo

--- make7z.sh
+++ make7z.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 # usage: make7z.sh 1.2.0
 set -x -e
-TOP_PATH=$(cd `dirname $0`; pwd) ;cd $TOP_PATH
-TMP_PATH=${TOP_PATH}/tmpbuild
-EXE7Z=${TOP_PATH}/tools/7za.exe
+MOD_NAME=MultiCheats
+TOP_PATH=$(cd `dirname $0`; pwd); cd $TOP_PATH
 
 version=$1
+sed -i 's!<Version value=.*$!<Version value="v'${version}'"/>!g' src/_Module/SubModule.xml
 
-sed -i 's!<Version value=.*$!<Version value="v'${version}'"/>!g' SubModule.xml
+rm -rf tmpbuild; mkdir -p tmpbuild
+cp -af src/_Module tmpbuild/${MOD_NAME}
 
-mkdir -p ${TMP_PATH}
-pushd ${TMP_PATH}
-    rm -rf MultiCheats
-    mkdir -p MultiCheats
+pushd tmpbuild/${MOD_NAME}
+	cp -af ${TOP_PATH}/pics ./
+	cp -af ${TOP_PATH}/LICENSE ./
+	cp -af ${TOP_PATH}/README.md ./
+	
+	${TOP_PATH}/tools/7za.exe a ../${MOD_NAME}-${version}.7z ./* -r
 popd
 
-pushd ${TMP_PATH}/MultiCheats
-    cp -af ${TOP_PATH}/bin ./
-    cp -af ${TOP_PATH}/ModuleData ./
-    cp -af ${TOP_PATH}/LICENSE ./
-    cp -af ${TOP_PATH}/README.md ./
-    cp -af ${TOP_PATH}/SubModule.xml ./
-    cp -af ${TOP_PATH}/src ./
-    
-    rm -rf src/.vs src/bin src/obj
-    rm -f src/MultiCheats.csproj.user
-    
-    ${EXE7Z} a ../MultiCheats-${version}.7z ./* -r
-popd
+
+# just for debug
+cp -af ${TOP_PATH}/src/obj/Debug/*.dll ${TOP_PATH}/tmpbuild/${MOD_NAME}/bin/Win64_Shipping_Client/
+cp -af ${TOP_PATH}/src/obj/Debug/*.pdb ${TOP_PATH}/tmpbuild/${MOD_NAME}/bin/Win64_Shipping_Client/
+rm -rf "/d/Games/steam/steamapps/common/Mount & Blade II Bannerlord/Modules/${MOD_NAME}"
+cp -af ${TOP_PATH}/tmpbuild/${MOD_NAME} "/d/Games/steam/steamapps/common/Mount & Blade II Bannerlord/Modules/${MOD_NAME}"
